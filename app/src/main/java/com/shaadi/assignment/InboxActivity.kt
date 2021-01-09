c7d43efac2766d04c6cc1dd6178b3b9ed31e8243
Bhavik Kasundra <con_bhavik.kasundra@Con-BhavikKasundra.local>
2021-01-09 14:13:19 +0530

    initial commit

---
+++ app/src/main/java/com/shaadi/assignment/InboxActivity.kt
@@ -0,0 +1,13 @@
+package com.shaadi.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InboxActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inbox)
+    }
+
+}
