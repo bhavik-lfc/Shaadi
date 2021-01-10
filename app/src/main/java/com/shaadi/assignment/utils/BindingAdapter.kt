5666ad1cb476fc259290883ea239b8ad6e0645e2
Bhavik Kasundra <con_bhavik.kasundra@Con-BhavikKasundra.local>
2021-01-10 22:48:00 +0530

    Text Updated

--- app/src/main/java/com/shaadi/assignment/utils/BindingAdapter.kt
+++ app/src/main/java/com/shaadi/assignment/utils/BindingAdapter.kt
@@ -1,9 +1,15 @@
 package com.shaadi.assignment.utils
 
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.shaadi.assignment.data.local.db.typeconverters.InvitationStatus
 
 @BindingAdapter("app:imageUrl")
 fun setImage(view: ImageView, url: String?) {
@@ -18,4 +24,25 @@ fun setImage(view: ImageView, url: String?) {
 @BindingAdapter("android:visibility")
 fun setVisibility(view: View, value: Boolean) {
     view.visibility = if (value) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("app:textStatus")
+fun setTextStatus(textView: TextView, invitationStatus: InvitationStatus) {
+
+    if (textView.visibility == View.VISIBLE) {
+        val spannableStringBuilder = SpannableStringBuilder()
+
+        val status = if (invitationStatus == InvitationStatus.ACCEPTED) "Accepted" else "Rejected"
+        val color = if (invitationStatus == InvitationStatus.ACCEPTED) Color.GREEN else Color.RED
+        val spannableString = SpannableString(status)
+        spannableString.setSpan(ForegroundColorSpan(color), 0, status.length, 0)
+
+        spannableStringBuilder.append("You have ")
+        spannableStringBuilder.append(spannableString)
+        spannableStringBuilder.append(" this user")
+
+        textView.setText(spannableStringBuilder, TextView.BufferType.SPANNABLE)
+
+    }
+
 }
