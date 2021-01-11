b3188008d7947c3c4f39393dad390fc6ebe70701
Bhavik Kasundra <con_bhavik.kasundra@Con-BhavikKasundra.local>
2021-01-11 22:48:20 +0530

    Error State

--- app/src/main/java/com/shaadi/assignment/ui/inbox/InboxViewModel.kt
+++ app/src/main/java/com/shaadi/assignment/ui/inbox/InboxViewModel.kt
@@ -1,17 +1,13 @@
 package com.shaadi.assignment.ui.inbox
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
 import com.shaadi.assignment.data.local.db.typeconverters.InvitationStatus
 import com.shaadi.assignment.data.repository.InboxRepository
 import com.shaadi.assignment.ui.base.BaseViewModel
-import com.shaadi.assignment.utils.Logger
 import com.shaadi.assignment.utils.NetworkHelper
 import com.shaadi.assignment.utils.SchedulerProvider
 import com.shaadi.assignment.utils.Utils
 import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 
 class InboxViewModel(
@@ -21,13 +17,9 @@ class InboxViewModel(
     private val inboxRepository: InboxRepository
 ) : BaseViewModel(schedulerProvider, compositeDisposable, networkHelper) {
 
-
-    companion object {
-        const val TAG = "InboxViewModel"
-    }
-
     val inboxUser = inboxRepository.getAll()
     val progressLoading: MutableLiveData<Boolean> = MutableLiveData(true)
+    val errorStatus: MutableLiveData<Boolean> = MutableLiveData(false)
 
     override fun onCreate() {
         getNews()
@@ -52,11 +44,11 @@ class InboxViewModel(
                 .doFinally {
                     progressLoading.postValue(false)
                 }
-                .subscribe({},
-                    {
-
+                .subscribe({}, {
+                    if (inboxUser.value.isNullOrEmpty()) {
+                        errorStatus.postValue(true)
                     }
-                )
+                })
         )
 
     }
